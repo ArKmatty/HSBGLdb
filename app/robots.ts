11bae590df596b5fbc6d76e606bd3e5ed50dfb90
ArKdestroy <mattiamonurro1@gmail.com>
2026-03-31 19:26:39 +0200

    Fix double slash in sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -2,12 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://your-domain.com');
+  const cleanBaseUrl = baseUrl.replace(/\/$/, '');
 
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: `${cleanBaseUrl}/sitemap.xml`,
   };
 }
